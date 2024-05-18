4a165afaf75010d4d4ac1df795da76069a44aa0f
nrak126 <daichidaichi126@gmail.com>
2024-05-19 00:10:01 +0900

    5.1

--- 05_10_hello1000.c
+++ 05_10_hello1000.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 int main(int argc, const char* argv[]) {
-  int sum = 0;
   for (int i = 0; i < 1000; i++) printf("Hello World\n");
   return 0;
 }
--- 05_challenge1_K24142.c
+++ 05_challenge1_K24142.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
-int main(int argc, const char* argv[]) {
-  int d, t;
+
+int main() {
+  int dayOfWeek;
+  int time;
+  int isOpen = 0;  // 0: 診察は行っていません, 1: 診察を行っています
+
   printf("曜日? ");
-  scanf("%d", &d);
+  scanf("%d", &dayOfWeek);
+
   printf("時間帯? ");
-  scanf("%d", &t);
+  scanf("%d", &time);
 
-  int s[2][7] = {{0, 1, 1, 1, 1, 1, 0}, {0, 1, 1, 0, 1, 1, 1}};
+  if (time == 0) {
+    if (dayOfWeek >= 1 && dayOfWeek <= 5) {
+      isOpen = 1;
+    }
+  } else if (time == 1) {
+    if ((dayOfWeek >= 1 && dayOfWeek <= 2) ||
+        (dayOfWeek >= 4 && dayOfWeek <= 6)) {
+      isOpen = 1;
+    }
+  }
 
-  if (s[t][d])
-    printf("診察を行なっています\n");
-  else
-    printf("診察を行なっていません\n");
+  if (isOpen == 1) {
+    printf("診察を行っています\n");
+  } else {
+    printf("診察は行っていません\n");
+  }
 
   return 0;
 }
