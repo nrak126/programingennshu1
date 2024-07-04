ca25e8dfe30766e46a60c0782b9985d0a7ac129f
nrak126 <daichidaichi126@gmail.com>
2024-07-04 10:17:04 +0900

    盤面の表示

--- src/final/main.c
+++ src/final/main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#define MAP_SIZE 20
 
-int main(int argc, char *argv[])
-{
-  
+void showMap(char map[][MAP_SIZE]) {
+    for(int i = 0; i < MAP_SIZE; i++) {
+        for(int j = 0; j < MAP_SIZE; j++)
+            printf("%c", map[i][j]);
 
-  return 0;
+        printf("\n");
+    }
+}
+
+int main(int argc, const char *argv[]) {
+    int timeOfMove = 0;
+    printf("ROTATION MAZE\n");
+    printf("一手の制限時間を指定してください...");
+    scanf("%d", &timeOfMove);
+
+    char map[MAP_SIZE][MAP_SIZE];
+
+    for(int i = 0; i < MAP_SIZE; i++) {
+        for(int j = 0; j < MAP_SIZE; j++) {
+            map[i][j] = '#';
+        }
+    }
+
+    showMap(map);
+
+    return 0;
 }
